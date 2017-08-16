89a9d9f62c3676cb5cdf6bb9919fae88b6518c3d
Cassie Butts <cassandralyb@gmail.com>
2017-08-16 17:56:55 -0500

    omdb

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,10 @@
-  
+ var CryptoJS = 
+ 
   $("#searchButton").on("click", function(event) {
     event.preventDefault();
     console.log("i clicked it");
+
+
 
 
 /* this is the person searched by the user when the user clickes the "user-search" button */
@@ -26,70 +29,33 @@
           }
           
         });
-/* this is the person searched by the user when the user clickes the "user-search" button */
-      var person = $("#searchInput").val().trim();
 
-      
+    var character = $("#searchInput").val().trim();
+    var queryURL = "http://www.omdbapi.com/?t=" + character + "&y=&plot=short&apikey=40e9cece";
 
-        var PRIV_KEY = "privatekey";
-        var API_KEY = "7b3c258aeabb823fef48eaa48dbfbbdc";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+        }).done(function(response) {
+        
+        var firstRow = $("table")
+        .children()
+        .eq(1)
+        .children("tr")
+        .eq(0)
+        .children("td");
 
-        function getMarvelResponse() {
-        // you need a new ts every request                                                                                    
-        var ts = new Date().getTime();
-        var hash = CryptoJS.MD5(ts + PRIV_KEY + API_KEY);
-      
-        // the api deals a lot in ids rather than just the strings you want to use
-        var characterId = '1009718'; // wolverine                                                                             
+         // api information set in table
+      firstRow.eq(0).text(response.Title);
 
-        var url = 'http://gateway.marvel.com:80/v1/public/comics';
-            alert(hash)
-            alert(url)
+      firstRow.eq(1).text(response.Actors);
 
-          console.log(url);
-              $.getJSON(url, {
-                ts: ts,
-                apikey: API_KEY,
-                hash: hash,
-                characters: characterId
-                })
-                .done(function(data) {
-                  // sort of a long dump you will need to sort through
-                  console.log(data);
-                })
-                .fail(function(err){
-                  // the error codes are listed on the dev site
-                  console.log(err);
-                });
-            };
-
-            getMarvelResponse();
-
-
- /* this is the AJAX call for the marvelURL */
-      $.ajax({
-          url: marvelURL,
-          method: "GET"
-        })
-        .done(function(response) {
-          console.log("marvelAPI");
-          var results = response.data;
-          for (var i = 0; i < results.length; i++) {
-            
-            var personImage = $("<img>");
-            personImage.attr("src", results[i].images.fixed_height.url);
-            $("#showGifs").prepend(personImage);
-            $(personImage).attr('id','resizeGif');
-           }
+      firstRow.eq(2).text(response.Plot);
           
         });
 
-        function changeImage() {
-        (document.getElementById("imgClickAndChange").src == "https://api.giphy.com/v1/gifs/search?q=" +
-        character + "&api_key=dc6zaTOxFJmzC&limit=1") 
-    }
           function refreshPage(){
             window.location.reload();
-    } 
-
-  });
+            }      
+    });
+ 
